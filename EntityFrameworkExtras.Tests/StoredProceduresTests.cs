9e43336bc8d34baa3fed82087130c4e39b07c6b7
Mike <fodsuk@hotmail.com>
2012-11-24 20:49:45 +0000

    - Included the code that will generate SqlParameters - Added a few Tests

---
+++ EntityFrameworkExtras.Tests/StoredProceduresTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using EntityFrameworkExtras;
+
+namespace EntityFrameworkExtras.Tests
+{
+    [TestClass]
+    public class StoredProceduresTests
+    {
+        DbContext context;
+
+        [TestInitialize]
+        public void Init()
+        {
+            context = new DbContext("ConnectionString");
+        }
+
+
+        [TestMethod]
+        public void Execute_Stored_Procedure_Without_Error()
+        {
+            var proc = new AddMemberStoredProcedure()
+            {
+                FirstName = "Michael",
+                LastName = "Rodda",
+                Age = 29
+            };
+
+            context.Database.ExecuteStoredProcedure(proc);
+
+            Assert.AreEqual(1, 1); //no exception occured            
+        }
+
+        [StoredProcedure("sp_AddMember")]
+        public class AddMemberStoredProcedure
+        {
+            [StoredProcedureParameter(SqlDbType.NVarChar)]
+            public string FirstName { get; set; }
+
+            [StoredProcedureParameter(SqlDbType.NVarChar)]
+            public string LastName { get; set; }
+
+            [StoredProcedureParameter(SqlDbType.Int)]
+            public int Age { get; set; }
+        }
+
+
+
+        [TestMethod]
+        public void Execute_Stored_Procedure_With_UDT_Without_Error()
+        {
+            var proc = new AddMemberStoredWithAddressesProcedure()
+            {
+                FirstName = "Craig",
+                LastName = "Bovis",
+                Age = 26,
+                Addresses = new List<Address>()
+                {
+                    new Address() {Line1 = "16", Line2 = "The Lane", Postcode = "MA24WE"}
+                }
+            };
+
+            context.Database.ExecuteStoredProcedure(proc);
+
+            Assert.AreEqual(1, 1); //no exception occured            
+        }
+
+        [StoredProcedure("sp_AddMemberWithAddresses")]
+        public class AddMemberStoredWithAddressesProcedure
+        {
+            [StoredProcedureParameter(SqlDbType.NVarChar)]
+            public string FirstName { get; set; }
+
+            [StoredProcedureParameter(SqlDbType.NVarChar)]
+            public string LastName { get; set; }
+
+            [StoredProcedureParameter(SqlDbType.Int)]
+            public int Age { get; set; }
+
+            [StoredProcedureParameter(SqlDbType.Udt)]
+            public List<Address> Addresses { get; set; }
+        }
+
+        [UserDefinedTableType("udt_Address")]
+        public class Address
+        {
+            [UserDefinedTableTypeColumn(1)]
+            public string Line1 { get; set; }
+
+            [UserDefinedTableTypeColumn(2)]
+            public string Line2 { get; set; }
+
+            [UserDefinedTableTypeColumn(3)]
+            public string Postcode { get; set; }
+        }
+
+
+
+
+        [TestMethod]
+        public void Execute_Stored_Procedure_With_Output_Parameter_Without_Error()
+        {
+            var proc = new GetMemberAgesStoredProcedure();
+
+            context.Database.ExecuteStoredProcedure(proc);
+
+            Assert.AreEqual(1, 1); //no exception occured            
+        }
+
+        [StoredProcedure("sp_GetOldestAge")]
+        public class GetMemberAgesStoredProcedure
+        {
+            [StoredProcedureParameter(SqlDbType.Int, Direction = ParameterDirection.Output)]
+            public int Age { get; set; }
+        }
+
+
+    }
+
+
+}
--- EntityFrameworkExtras/StoredProcedureParser.cs
+++ EntityFrameworkExtras/StoredProcedureParser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 
@@ -19,11 +20,16 @@ namespace EntityFrameworkExtras
 
             string sql = BuildSql(storedProcedureName, parameterInfo);
 
-            return new StoredProcedureInfo(storedProcedureName)
+            var info = new StoredProcedureInfo(storedProcedureName)
                 {
                     Sql = sql,
-                    ParameterInfos = parameterInfo
+                    ParameterInfos = parameterInfo,
                 };
+
+            info.SqlParameters = BuildSqlParameters(storedProcedure, info);
+
+            return info;
+            
         }
 
         public static string GetStoreProcedureName(object storedProcedure)
@@ -59,7 +65,7 @@ namespace EntityFrameworkExtras
                             IsMandatory = isMandatory,
                             SqlDataType = attribute.DataType,
                             PropertyInfo = propertyInfo,
-                            Direction = attribute.Direction
+                            Direction = attribute.Direction                           
                         });
                 }
             }
@@ -74,6 +80,52 @@ namespace EntityFrameworkExtras
 
             return execSql + parameterSql;
         }
+
+        private static SqlParameter[] BuildSqlParameters(object storedProcedure, StoredProcedureInfo info)
+        {
+            var sqlParams = new List<SqlParameter>();
+
+            foreach (var p in info.ParameterInfos)
+            {
+                var propertyValue = p.PropertyInfo.GetValue(storedProcedure, null);
+
+                var value = p.IsUserDefinedTable
+                                         ? _helper.GetUserDefinedTableValue(p.PropertyInfo, storedProcedure)
+                                         : propertyValue;
+
+                SqlParameter sqlParameter = GenerateSqlParameter(p.Name,
+                                                                 value,
+                                                                 p.IsMandatory,
+                                                                 p.IsUserDefinedTable,
+                                                                 p.IsUserDefinedTable ?
+                                                                                    _helper.GetUserDefinedTableType(p.PropertyInfo) : null,
+                                                                p.SqlDataType,
+                                                                p.Direction);
+
+                sqlParams.Add(sqlParameter);
+            }
+
+            return sqlParams.ToArray();
+        }
+
+        private static SqlParameter GenerateSqlParameter(string parameterName, object paramValue, bool mandatory,
+                                           bool isUserDefinedTableParameter, string udtType, SqlDbType dataType, ParameterDirection direction)
+        {
+            var sqlParameter = new SqlParameter("@" + parameterName, paramValue ?? DBNull.Value)
+            {
+                Direction = direction,
+                IsNullable = !mandatory
+            };
+
+            if (isUserDefinedTableParameter)
+                sqlParameter.TypeName = udtType;
+            else
+                sqlParameter.SqlDbType = dataType;
+
+            return sqlParameter;
+        }
+
+
 
     }
 }
