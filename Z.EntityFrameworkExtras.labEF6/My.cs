9864bb359ced05bc6a24ca0bf24e12270f1a3648
lempireqc <mikael.lanouette@gmail.com>
2020-02-21 09:05:20 -0500

    save add lab

---
+++ Z.EntityFrameworkExtras.labEF6/My.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.EntityFrameworkExtras.labEF6
+{ 
+	class My
+	{
+		public static string DataBaseName = "LabExtra";
+		  
+		public static string ConnectionString =
+			("Server=[REPLACE];Initial Catalog = [BD]; Integrated Security = true; Connection Timeout = 300; Persist Security Info=True").Replace("[REPLACE]", Environment.MachineName).Replace("[BD]", DataBaseName);
+
+		public static void DeleteBD(DbContext context)
+		{
+			context.Database.Delete();
+		}
+
+		public static void CreateBD(DbContext context)
+		{
+			try
+			{
+				context.Database.CreateIfNotExists();
+				if (!context.Database.CompatibleWithModel(throwIfNoMetadata: true))
+				{
+					throw new Exception("Delete and Create DataBase");
+				}
+			}
+			catch
+			{
+				try
+				{
+					My.DeleteBD(context);
+				}
+				catch (Exception e)
+				{
+					using (var commande = new SqlCommand("ALTER DATABASE " + DataBaseName + " SET SINGLE_USER WITH ROLLBACK IMMEDIATE;DROP DATABASE " + DataBaseName + " ;", new SqlConnection(My.ConnectionString)))
+					{
+						commande.Connection.Open();
+						commande.ExecuteNonQuery();
+					}
+				}
+				context.Database.CreateIfNotExists();
+			}
+		}
+	}
+} 
---
+++ Z.EntityFrameworkExtras.labEF6/Request_OutputParam.cs
@@ -0,0 +1,121 @@
+﻿using EntityFrameworkExtras.EF6;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.EntityFrameworkExtras.labEF6
+{
+	class Request_OutputParam
+	{ 
+		public static void Execute()
+		{
+			// Create BD 
+			using (var context = new EntityContext())
+			{
+				My.CreateBD(context);
+			}
+
+			// CLEAN  
+			using (var context = new EntityContext())
+			{
+				context.EntitySimples.RemoveRange(context.EntitySimples);
+
+				context.SaveChanges();
+			}
+
+			// SEED  
+			using (var context = new EntityContext())
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					context.EntitySimples.Add(new EntitySimple { ColumnInt = i });
+				}
+
+				context.SaveChanges();
+			}
+
+
+
+			// TEST  
+			using (var context = new EntityContext())
+			{
+				var connection = context.Database.Connection;
+				connection.Open();
+				using (var commande = connection.CreateCommand())
+				{
+					commande.CommandText = @"   
+if exists (select 1 from sys.procedures where name = 'PROC_Get_EntitySimple')
+BEGIN
+DROP PROCEDURE [dbo].[PROC_Get_EntitySimple]
+END 
+						";
+					commande.ExecuteNonQuery();
+				}
+
+				using (var commande = connection.CreateCommand())
+				{
+					commande.CommandText = @"    
+CREATE PROCEDURE [dbo].[PROC_Get_EntitySimple]
+
+	@ParameterID INT 	,
+	@ParameterInt INT = NULL OUTPUT 
+
+
+AS
+BEGIN 
+Select * from EntitySimples
+Where ColumnInt = @ParameterID
+
+Set @ParameterInt = @ParameterID +1 
+END
+						";
+					commande.ExecuteNonQuery();
+				}
+
+			}
+
+			// TEST  
+			using (var context = new EntityContext())
+			{
+				var proc_Get_EntitySimple = new Proc_Get_EntitySimple() { ParameterID = 2 };
+				var entity = context.Database.ExecuteStoredProcedure<EntitySimple>(proc_Get_EntitySimple);
+				var output = proc_Get_EntitySimple.ParameterInt;
+			}
+		}
+
+		public class EntityContext : DbContext
+		{
+			public EntityContext() : base(My.ConnectionString)
+			{
+			}
+
+			public DbSet<EntitySimple> EntitySimples { get; set; }
+
+			protected override void OnModelCreating(DbModelBuilder modelBuilder)
+			{
+				base.OnModelCreating(modelBuilder);
+			}
+		}
+
+		[StoredProcedure("PROC_Get_EntitySimple")]
+		public class Proc_Get_EntitySimple
+		{
+			[StoredProcedureParameter(SqlDbType.Int, Direction = ParameterDirection.Output)]
+			public int ParameterInt { get; set; }
+
+			[StoredProcedureParameter(SqlDbType.Int, Direction = ParameterDirection.Input)]
+			public int ParameterID { get; set; }
+		}
+
+		public class EntitySimple
+		{
+			public int ID { get; set; }
+			public int ColumnInt { get; set; }
+			public String ColumnString { get; set; }
+		}
+	}
+}
