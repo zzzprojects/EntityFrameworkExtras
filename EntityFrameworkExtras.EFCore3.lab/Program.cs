372d0834993706c5bb40b1bcbd90acd7e4b89a51
lempireqc <mikael.lanouette@gmail.com>
2020-03-20 08:39:07 -0400

    save

---
+++ EntityFrameworkExtras.EFCore3.lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Z.EntityFrameworkExtras.Lab.EFCore30
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Request_storeProcedure_Transaction.Execute();
+		}
+	}
+}
